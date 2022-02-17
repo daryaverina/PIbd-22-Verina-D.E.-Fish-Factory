e8ce01bd3f37c0ba50a88c7a4f5d546561330492
Daria Verina <dashka400g@gmail.com>
2022-02-17 15:57:16 +0400

    2 laba

---
+++ FishFactory/FishFactoryFileImplement/Models/Canned.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FishFactoryFileImplement.Models
+{
+    /// Изделие, изготавливаемое в магазине
+    public class Canned
+    {
+        public int Id { get; set; }
+        public string CannedName { get; set; }
+        public decimal Price { get; set; }
+        public Dictionary<int, int> CannedComponents { get; set; }
+    }
+}
